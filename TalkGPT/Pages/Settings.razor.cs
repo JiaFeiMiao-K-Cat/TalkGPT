3fc3f3d67a7e176408d64d18463c826f9305e119
SaoJiaFei <848418127@qq.com>
2023-06-10 17:35:47 +0800

    add json mode for settings

--- TalkGPT/Pages/Settings.razor.cs
+++ TalkGPT/Pages/Settings.razor.cs
@@ -3,6 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 using TalkGPT.Utils;
 
@@ -10,10 +13,25 @@ namespace TalkGPT.Pages
 {
     public partial class Settings
     {
+        private string JsonText { get 
+            {
+                return JsonSerializer.Serialize(_sdkSettings.SDKSettings, new JsonSerializerOptions
+                    {
+                        WriteIndented = true,
+                        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All)
+                    }
+                );
+            } 
+            set
+            {
+                _sdkSettings.SDKSettings = JsonSerializer.Deserialize<SDKSettings>(value);
+            }
+        }
         void SaveAsync()
         {
-            _sdkSettings.SaveSettings();
+            // change order for illegal settings will make program crash but not save to file
             _gptSDK.RefreshSettings();
+            _sdkSettings.SaveSettings();
         }
     }
 }
